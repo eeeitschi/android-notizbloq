980c3d6bf9f2e6936f80009d67fec1205843215d
Achim Grether <a.grether@hotmail.com>
2022-05-16 17:58:37 +0200

    add photo preview in note viewer

--- app/src/main/java/com/example/notizbloq_v2/NoteViewer.java
+++ app/src/main/java/com/example/notizbloq_v2/NoteViewer.java
@@ -1,29 +1,27 @@
 package com.example.notizbloq_v2;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NoteViewer extends AppCompatActivity {
 
     EditText noteTitle, noteText; // Views in diesem Layout
     String noteFileName;
     Note loadedNote; // mitgegebene Note
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +37,43 @@ public class NoteViewer extends AppCompatActivity {
         noteFileName = intent.getStringExtra("NOTE_FILE");
 
         // Wenn eine Note mitgegeben wurde, die entsprechende Notiz aus dem Speicher holen und laden
-        if (noteFileName != null && !noteFileName.isEmpty() ) {
+        if (noteFileName != null && !noteFileName.isEmpty()) {
             loadedNote = Utilities.getNoteByName(this, noteFileName);
 
-            if(loadedNote != null) {
+            if (loadedNote != null) {
                 noteTitle.setText(loadedNote.getNoteTitle());
                 noteText.setText(loadedNote.getNoteText());
             }
         }
 
+        // Add onclicklistener for the take photo button
+        Button button = (Button) findViewById(R.id.btnTakePhoto);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // start take picture intent.
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.imageViewPictureThumbnail);
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
